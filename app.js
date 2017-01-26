19235ece9b77dcf5c1a100b182227fffff649d2d
dexuiz <dex345z@gmail.com>
2017-01-27 01:05:37 +0530

    backend and description added

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 var  express =require("express");
 var  app = express();
-app.set("view engine","ejs");
 var bodyParser= require("body-parser");
+var mongoose = require("mongoose");
+app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost/yelp-camp");
+//schema Setup
+
+var schema = new mongoose.Schema({
+  name:String,
+  image:String,
+  desc:String
+})
+
+var campground = mongoose.model("campground",schema);
+
+// campground.create({
+//   name:"lakes",
+//   image:"http://www.escapehere.com/wp-content/uploads/2013/07/820x480xBlue-Lake-Australia-820x480.jpg.pagespeed.ic.ewgsVxcoEV.jpg",
+//   desc:"kfojfijijufrin8dui8cjnfdiutuneirhtnurtyytr"
+// },function(err,grounds){
+//   if(err)
+//     console.log("error is here",err);
+//   else {
+//     console.log(grounds);
+//   }
+// });
 
 var grounds =[
   {name:'mountains' ,image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/K%C3%B6lpinsee_bei_Jabel.jpg/330px-K%C3%B6lpinsee_bei_Jabel.jpg"},
@@ -16,16 +39,41 @@ app.get("/",function(req,res){
 });
 
 app.get("/grounds",function(req,res){
-  res.render("campgrounds",{grounds:grounds});
+  campground.find({},function(err,campgrounds){
+    if(err){
+      console.log("error detected");
+    }else {
+      res.render("campgrounds",{grounds:campgrounds});
+    }
+  })
 })
 
 app.post("/grounds",function(req,res){
- console.log("You accessed the post route");
- grounds.push({name:req.body.newName , image:req.body.newImage});
- res.redirect("/grounds");
+ console.log(req.body.newDesc);
+
+ campground.create({name:req.body.newName , image:req.body.newImage ,desc:req.body.newDesc},function(err,campgrounds){
+   if(err)
+      console.log("error generated",err);
+    else {
+      console.log("add done");
+      res.redirect("/grounds");
+
+    }
+ });
 })
 app.get("/grounds/new",function(req,res){
   res.render("new");
+})
+
+app.get("/grounds/:id",function(req,res){
+   campground.findById(req.params.id,function(err,foundGround){
+    if(err){
+      console.log("error occured",err);
+    }else {
+      res.render("show",{ground:foundGround});
+    }
+  });
+
 })
 
 app.listen(3000,function(){
